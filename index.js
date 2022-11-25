aa2d2491aa654c552b26d9afccc915793e46bf06
basiliobarbosaESTG <basiliobarbosa@ipvc.pt>
2022-11-25 16:57:03 +0000

    express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("eu sou o basilio")
+
+const express = require("express")
+
+const app = express()
+
+app.listen(3000, () => {
+    console.log(`server running on port ${3000}`)
+})
+
+app.get("/user", function(req, res){ /**req cliente vai mandar para servidor e res é o que o servidor manda */
+    res.send("eu sou o buzz")
+})
+
